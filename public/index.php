5e06af3329626e6238db7f4a7d66f15725e41550
rami rayya <rami.k.rayya@gmail.com>
2024-01-05 11:16:23 +0200

    Configured Autoloader & App Namespace.

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Tasko\Helpers\Helper;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
@@ -9,7 +10,10 @@ require __DIR__ . '/../vendor/autoload.php';
 $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
+    $message = Helper::sayHello("Tasko");
+
+    $response->getBody()->write($message);
+
     return $response;
 });
 
---
+++ src/Helpers/Helper.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Tasko\Helpers;
+
+class Helper {
+  public static function sayHello(string $name) {
+    return "hello $name";
+  }
+}
